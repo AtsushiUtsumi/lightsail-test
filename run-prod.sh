59195918abf7cb965fa4a19344ddc13bb78ea50e
AtsushiUtsumi <debtgx103@gmail.com>
2026-01-23 02:01:54 +0900

    コマンドのミスを修正

--- run-prod.sh
+++ run-prod.sh
@@ -2,5 +2,5 @@
 clear
 docker rm -f $(docker ps -aq) 2>/dev/null || true
 clear
-docker-compose -f docker-compose.prod.yml pull
-docker-compose -f docker-compose.prod.yml up
+docker compose -f docker-compose.prod.yml pull
+docker compose -f docker-compose.prod.yml up
